c068c45b6457e3bf414c0d24c9ccf3cf27e22bc3
Capoaleman <57103471+Capoaleman@users.noreply.github.com>
2019-12-01 20:39:38 +0100

    Add files via upload

---
+++ day1aventcal2019.py
@@ -0,0 +1,15 @@
+def comb_cal(mass):
+    res = mass//3-2
+    if res <= 0:
+        return 0
+    else:
+        return res + comb_cal(res)
+
+
+if __name__ == "__main__":
+    total_fuel = 0
+    f = open("./aventcal2019/inputday1.txt", "r")
+    filename = f.readlines()
+    for line in filename:
+        total_fuel += comb_cal(int(line))
+    print(total_fuel)

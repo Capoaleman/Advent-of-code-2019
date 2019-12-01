82c029fa31b15d301538a0f606f9680782eaecb4
Capoaleman <57103471+Capoaleman@users.noreply.github.com>
2019-12-01 20:54:32 +0100

    Update day1aventcal2019.py

--- day1aventcal2019.py
+++ day1aventcal2019.py
@@ -1,3 +1,8 @@
+# Function for the 1st part os the challenge
+def comb_cal(mass):
+    return mass//3-2
+
+# Function for the 2nd part of the challenge
 def comb_cal(mass):
     res = mass//3-2
     if res <= 0:
@@ -5,7 +10,7 @@ def comb_cal(mass):
     else:
         return res + comb_cal(res)
 
-
+# Main stays the same for both parts
 if __name__ == "__main__":
     total_fuel = 0
     f = open("./inputday1.txt", "r")

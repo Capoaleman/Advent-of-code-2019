7753e88fea6e1ddc8307e8d62619307e52aef08b
Capoaleman <57103471+Capoaleman@users.noreply.github.com>
2019-12-19 22:57:18 +0100

    Add files via upload

---
+++ day19adventcal2019.py
@@ -0,0 +1,141 @@
+from copy import deepcopy
+
+
+def intercode_program(arr, i=0, rel_base=0, entry=0):
+    resul = 0
+    while i <= len(arr):
+        # Add
+        if arr[i] % 100 == 1:
+            par1, par2, par3 = arr[i+1:i+4]
+            mod_1par = arr[i] // 100 % 10
+            mod_2par = arr[i] // 1000 % 10
+            mod_3par = arr[i] // 10000 % 10
+            x1 = par1 if mod_1par == 1 else arr[rel_base +
+                                                par1] if mod_1par == 2 else arr[par1]
+            x2 = par2 if mod_2par == 1 else arr[rel_base +
+                                                par2] if mod_2par == 2 else arr[par2]
+            x3 = rel_base + par3 if mod_3par == 2 else par3
+            arr[x3] = x1+x2
+            i += 4
+        # Multiply
+        elif arr[i] % 100 == 2:
+            par1, par2, par3 = arr[i+1:i+4]
+            mod_1par = arr[i] // 100 % 10
+            mod_2par = arr[i] // 1000 % 10
+            mod_3par = arr[i] // 10000 % 10
+            x1 = par1 if mod_1par == 1 else arr[rel_base +
+                                                par1] if mod_1par == 2 else arr[par1]
+            x2 = par2 if mod_2par == 1 else arr[rel_base +
+                                                par2] if mod_2par == 2 else arr[par2]
+            x3 = rel_base + par3 if mod_3par == 2 else par3
+            arr[x3] = x1*x2
+            i += 4
+        # get input
+        elif arr[i] % 100 == 3:
+            mod_1par = arr[i] // 100 % 10
+            par1 = arr[i+1]
+            resul = rel_base + par1 if mod_1par == 2 else par1
+            arr[resul] = entry
+            i += 2
+            return None, True, i, rel_base
+        # set output
+        elif arr[i] % 100 == 4:
+            mod_1par = arr[i] // 100 % 10
+            par1 = arr[i+1]
+            resul = par1 if mod_1par == 1 else arr[rel_base +
+                                                   par1] if mod_1par == 2 else arr[par1]
+            i += 2
+            return resul, True, i, rel_base
+
+        # Jump-if-True
+        elif arr[i] % 100 == 5:
+            mod_1par = arr[i] // 100 % 10
+            mod_2par = arr[i] // 1000 % 10
+            par1, par2 = arr[i+1:i+3]
+            x1 = par1 if mod_1par == 1 else arr[rel_base +
+                                                par1] if mod_1par == 2 else arr[par1]
+            x2 = par2 if mod_2par == 1 else arr[rel_base +
+                                                par2] if mod_2par == 2 else arr[par2]
+            i = x2 if x1 != 0 else i+3
+        # Jump-if-False
+        elif arr[i] % 100 == 6:
+            mod_1par = arr[i] // 100 % 10
+            mod_2par = arr[i] // 1000 % 10
+            par1, par2 = arr[i+1:i+3]
+            x1 = par1 if mod_1par == 1 else arr[rel_base +
+                                                par1] if mod_1par == 2 else arr[par1]
+            x2 = par2 if mod_2par == 1 else arr[rel_base +
+                                                par2] if mod_2par == 2 else arr[par2]
+            i = x2 if x1 == 0 else i+3
+        # less than
+        elif arr[i] % 100 == 7:
+            par1, par2, par3 = arr[i+1:i+4]
+            mod_1par = arr[i] // 100 % 10
+            mod_2par = arr[i] // 1000 % 10
+            mod_3par = arr[i] // 10000 % 10
+            x1 = par1 if mod_1par == 1 else arr[rel_base +
+                                                par1] if mod_1par == 2 else arr[par1]
+            x2 = par2 if mod_2par == 1 else arr[rel_base +
+                                                par2] if mod_2par == 2 else arr[par2]
+            x3 = rel_base + par3 if mod_3par == 2 else par3
+            arr[x3] = 1 if x1 < x2 else 0
+            i += 4
+        # equals
+        elif arr[i] % 100 == 8:
+            par1, par2, par3 = arr[i+1:i+4]
+            mod_1par = arr[i] // 100 % 10
+            mod_2par = arr[i] // 1000 % 10
+            mod_3par = arr[i] // 10000 % 10
+            x1 = par1 if mod_1par == 1 else arr[rel_base +
+                                                par1] if mod_1par == 2 else arr[par1]
+            x2 = par2 if mod_2par == 1 else arr[rel_base +
+                                                par2] if mod_2par == 2 else arr[par2]
+            x3 = rel_base + par3 if mod_3par == 2 else par3
+            arr[x3] = 1 if x1 == x2 else 0
+            i += 4
+        # relative base
+        elif arr[i] % 100 == 9:
+            par1 = arr[i+1]
+            mod_1par = arr[i] // 100 % 10
+            x1 = par1 if mod_1par == 1 else arr[rel_base +
+                                                par1] if mod_1par == 2 else arr[par1]
+            rel_base = rel_base+x1
+            i += 2
+        elif arr[i] == 99:
+            print("Halt 99 opcode")
+            return resul, False, 0, rel_base
+        else:
+            raise ValueError("Unknown instruction {}".format(arr[i]))
+    return
+
+
+def check_beam(x, y):
+    """ Checks the droid in the point x, y """
+    code = deepcopy(input_arr)
+    i = 0
+    rel_base = 0
+    res, flag, i, rel_base = intercode_program(
+        code, i, rel_base, x)
+    res, flag, i, rel_base = intercode_program(
+        code, i, rel_base, y)
+    affect, flag, i, rel_base = intercode_program(
+        code, i, rel_base)
+    return affect
+
+
+if __name__ == "__main__":
+    f = open("./day 19/inputday19.txt", "r")
+    input_arr = list(map(int, f.readline().split(",")))
+    for i in range(30):
+        input_arr.append(0)
+    # part one of the challenge
+    print(sum(check_beam(x, y) for x in range(50) for y in range(50)))
+
+    # part two of the challenge
+    x = y = 0
+    while check_beam(x, y+99) == 0:
+        x += 1
+        if check_beam(x+99, y) == 0:
+            y += 1
+    print(f"The value is {x*10000+y}")
+    print(f"x={x}, y={y}")
